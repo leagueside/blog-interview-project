97217955b786a77bfc311034c3aabfb37ab9bdc7
Jon Thompson <jt@leagueside.com>
2019-06-13 16:41:18 -0400

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+jt = User.create name: 'JT'
+shannon = User.create name: 'Shannon'
+stephen = User.create name: 'Stephen'
+
+pokemon = Blog.create title: 'West Philly Pokemon', user: shannon
+cats = Blog.create title: 'Cutest Cats', user: jt
+
+bulbasaur = Post.create title: 'Bulbasaur Found!', content: '40th & Market', blog: pokemon
+Post.create title: 'Charizard Found!', content: '42nd & Chestnut', blog: pokemon
+monday_cat = Post.create title: 'Monday Cute Cate', content: 'https://placekitten.com/300/400', blog: cats
+
+Favorite.create post: bulbasaur, user: jt
+Favorite.create post: monday_cat, user: stephen
+Favorite.create post: monday_cat, user: shannon
+
+Log.create entry: 'Database seeded!'
