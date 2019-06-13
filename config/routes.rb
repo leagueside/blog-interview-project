74bee5842efac00401cdc8192acde7a434a4ec72
Jon Thompson <jt@leagueside.com>
2019-06-13 16:27:32 -0400

    Scaffold Logs model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :logs
   resources :favorites
   resources :posts
   resources :blogs
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_202437) do
+ActiveRecord::Schema.define(version: 2019_06_13_202702) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 2019_06_13_202437) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_favorites_on_post_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "logs", force: :cascade do |t|
+    t.string "entry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
